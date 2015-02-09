ddf09cb04ac32aa1473fc8999996239d5114d048
Michael Foody <mfoody@hotmail.com>
2015-02-09 23:40:05 +1100

    dropped database to fix migrations

---
+++ db/migrate/20150209115401_drop_reservations.rb
@@ -0,0 +1,4 @@
+class DropReservations < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209110405) do
+ActiveRecord::Schema.define(version: 20150209115401) do
 
   create_table "bikes", force: :cascade do |t|
     t.string   "title"
@@ -46,12 +46,12 @@ ActiveRecord::Schema.define(version: 20150209110405) do
   add_index "comments", ["bike_id"], name: "index_comments_on_bike_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
-  create_table "reviews", force: :cascade do |t|
+  create_table "reservations", force: :cascade do |t|
+    t.string   "start_date"
+    t.string   "end_date"
+    t.integer  "bike_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "rating"
-    t.integer  "user_id"
-    t.integer  "bike_id"
   end
 
   create_table "users", force: :cascade do |t|
